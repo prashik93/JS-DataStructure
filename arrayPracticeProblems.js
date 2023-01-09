252756b0d665a7e17c1b0c25f3b7fe0832e40fcf
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-09 21:08:11 +0530

    ADDED : Q1.b Store 10 Random 3 Digit Numbers Into An Array

--- arrayPracticeProblems.js
+++ arrayPracticeProblems.js
@@ -1,7 +1,19 @@
+let randomElementArray = new Array();
+
 function generateTenRandomThreeDigitNumber() {
     for(let i = 0; i < 10; i++) {
         let randomNumber = Math.floor(Math.random() * (999 - 100 + 1) + 100);
-        console.log("Random Number :- " + randomNumber);
+        // console.log("Random Number :- " + randomNumber);
+        randomElementArray.push(randomNumber);
     }
 }
 generateTenRandomThreeDigitNumber();
+
+let txt = "";
+function arrayIterationFunction(value) {
+    txt += value + " ";
+}
+
+randomElementArray.forEach(arrayIterationFunction);
+
+console.log("Elements Present In An Array :- " + txt);
